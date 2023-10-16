0e0b8ec3fc805c55c7d2a41f3339ff1d59cd7656
Ike Addy <ikaddy05@gmail.com>
2023-10-16 18:49:06 -0400

    Started project over while referencing web dev simplified's tutorial Created new Board Added new styling Added X's and O's Next is to add on hover effect

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const startBtn = document.querySelector('.start-btn');
-const restartBtn = document.querySelector('.restart-btn')
-const cells = document.querySelector('#cells-container');
-
-cells.addEventListener('click', () => {
-    alert('hello world!')
-})
