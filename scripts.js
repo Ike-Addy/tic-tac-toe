e6a02101051d3a6ce0796224c93775e47fa81d3c
Ike Addy <ikaddy05@gmail.com>
2024-01-30 07:01:24 -0500

    Created player object Working on game logic

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
-((num = 5) => {
-    console.log(num);
+// Controls gameboard
+(() => {
+    let gameBoard;
 })();
+
+// Controls current player and symbol display
+(() => {
+    const playerOne = player('Player One', 'X');
+    const playerTwo = player('Player Two', 'O');
+    let displayController
+})();
+
+// Controls players
+function player (name, symbol) {
+    return { name, symbol };
+}
+
+
